2932fdb1d460ae0832ac058f27305bbbb87158b3
vvalennn22 <puentevalentino@gmail.com>
2025-04-30 11:57:23 +0000

    tecno

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-variable=1
 variable="gabriel"
-variable=true
-variable=none
-print(variable)
+variable=True
+variable=None
+variable=1
+comparacion=variable==1
+print(comparacion, "gabri")
+comparacion=not comparacion
+print(comparacion, "not")
+mayor=variable>0
+print(mayor, "mayor")
+menor=variable<0
+print(menor, "menor")
+mayor=(1+1)>1
+menor=(20-30)<10
+y=mayor and menor 
+print(y,"and")
